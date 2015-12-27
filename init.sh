892d4d3ce266a040b3db1e3b1f0a000ef3cf4749
CristhianMotoche <cristhian.motoche@gmail.com>
2015-12-27 12:30:29 -0500

    Add advices

--- init.sh
+++ init.sh
@@ -6,3 +6,7 @@ cp -r * ~/.config/nvim
 
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo "Don't forget to do:"
+echo ":PlugInstall"
+echo ":UpdateRemotePlugins"
