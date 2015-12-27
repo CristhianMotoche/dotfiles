8b7b1965f354d2a6903c22f9eec68ef9c41272e7
CristhianMotoche <cristhian.motoche@gmail.com>
2015-12-27 11:41:59 -0500

    Change gitignore and add pluggins

--- init.sh
+++ init.sh
@@ -1,2 +1,8 @@
+cd /tmp
+git clone https://github.com/Shougo/deoplete.nvim.git
+cd deoplete.nvim/
+rm -rf .git .gitignore
+cp * ~/.config/nvim
+
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
