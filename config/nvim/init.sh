ee7a20cf66924146e4708398f938051a9d5db29a
CristhianMotoche <cristhian.motoche@gmail.com>
2016-06-28 12:51:37 -0500

    Update init.sh

--- config/nvim/init.sh
+++ config/nvim/init.sh
@@ -3,6 +3,11 @@ git clone https://github.com/Shougo/deoplete.nvim.git
 cd deoplete.nvim/
 rm -rf .git .gitignore test
 cp -r * ~/.config/nvim
+cd ..
+git clone https://github.com/craigemery/vim-autotag.git
+cd vim-autotag/
+mv plugin ~/.config/nvim/pluggins
+
 
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
