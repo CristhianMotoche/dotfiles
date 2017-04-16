2c4497f36bd88a550e9f04d35cf7d01caaa11d48
CristhianMotoche <cristhian.motoche@gmail.com>
2017-04-16 09:47:43 -0500

    Update links shell script

--- links.sh
+++ links.sh
@@ -4,15 +4,23 @@ echo "Start!"
 
 for item in $(ls "${DIR}")
 do
-  echo ${item}
-  ln -s "${DIR}/${item}" "${HOME}/.${item}"
+	echo ${item}
+	if [[ "${item}" == "config" || "${item}" == "my_bin" ]]; then
+		# Do nothing
+		echo "Skip directory... Will be linked later"
+	else
+		ln -s "${DIR}/${item}" "${HOME}/.${item}"
+	fi
 done
+
+ln -s "${DIR}/my_bin" "${HOME}/.my_bin"
+ln -s "${DIR}/config/nvim" "${HOME}/.config/nvim"
 
 toDelete=(README.markdown links.sh programs.sh)
 
 for item in ${toDelete}
 do
-  rm "${HOME}/.${item}"
+	rm "${HOME}/.${item}"
 done
 
 echo "Finish!"
