0b24ef8b75d7b20e90ca422aa9c269629d82cba3
CristhianMotoche <cristhian.motoche@gmail.com>
2019-06-11 20:49:04 -0500

    Remove unneeded scripts

--- programs.sh
+++
@@ -1,8 +0,0 @@
-# INSTALL ALL THE SOFTWARE YOU NEED
-
-PROGRAMS_PACMAN="yaourt zsh evolution shutter xclip keepassx tmux ctags vagrant stack"
-PROGRAMS_YAOURT="evolution-on-git dbvis insync google-chrome python-neovim the_silver_searcher google-talkplugin"
-
-sudo pacman -S $PROGRAMS_PACMAN --noconfirm
-yaourt -S $PROGRAMS_YAOURT --noconfirm
-
